c23f19631b3d28cc0b1a7a4a37102983962c4ca1
Ahmed Rifat <rifatbafsc@gmail.com>
2022-03-10 23:58:02 +0600

    user and user comment error fixed

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-  console.log(text);
+  // console.log(text);
     return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
@@ -53,7 +53,7 @@ const switchTab = (id) => {
 
 const createPost = (post) => {
     const image = post;
-    // console.log(post.userImage);
+    // console.log(post.comments[0].user);
     const div = document.createElement( "article" );
     div.classList.add( "post" );
     div.innerHTML = `
@@ -122,9 +122,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${image.comments[0].user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0].text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
