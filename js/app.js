8d907fb56fd9e901d31f1e03b503512c156c21df
Ahmed Rifat <rifatbafsc@gmail.com>
2022-03-11 00:17:26 +0600

    Liked and Report double post add error fixed

--- js/app.js
+++ js/app.js
@@ -145,14 +145,17 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById( "liked" ).innerHTML = "";
     const likedPosts = getLikedPosts();
     likedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "liked" ).appendChild(div);
+        
     });
 };
 
 const displayReportedPosts = () => {
+  document.getElementById( "reported" ).innerHTML= "";
     const reportedPosts = getReportedPosts();
     reportedPosts.forEach((post) => {
         const div = createPost(post);
